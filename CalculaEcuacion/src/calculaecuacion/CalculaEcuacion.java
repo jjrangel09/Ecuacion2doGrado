8e9dfa930e72d62aa4d57d87f5c58f470aafe5b2
Juan José Rangel <juanjose840@yahoo.com.co>
2018-02-20 00:03:03 -0500

    Version 1.2
    Se agrega demostracion del ejercicio

--- CalculaEcuacion/src/calculaecuacion/CalculaEcuacion.java
+++ CalculaEcuacion/src/calculaecuacion/CalculaEcuacion.java
@@ -21,13 +21,23 @@ public class CalculaEcuacion {
         byte c = 12;
         byte x1 ;
         byte x2 ;
+        byte raiz = (byte) Math.sqrt(Math.pow(b, 2)-(4*a*c));
+        byte temp;
         
-        x1 = (byte) ((byte) (((byte) -b + Math.sqrt((byte)Math.pow(b, 2)-(4*(a+c)))))/(2*a)); 
-        x2 = (byte) ((byte) (((byte) -b - Math.sqrt((byte)Math.pow(b, 2)-(4*(a+c)))))/(2*a)); 
+        
+        x1 = (byte) (( -b + raiz)/(2*a)); 
+        x2 = (byte) (( -b - raiz)/(2*a)); 
         
         System.out.println("Calculo de Ecuacion de Segundo Grado\nX^2+8X+12=0\n");
         System.out.println("Primera solucion: X1= " + x1);
         System.out.println("Segunda solucion: X2= " + x2 + "\n");
+        
+        System.out.println("COMPROBACION:\n");
+        temp = (byte) (Math.pow(x1,2)+(8*x1)+12);
+        System.out.println(x1 +"^2+8" + "(" + x1 + ")" + "+12=" + temp);
+ 
+        temp = (byte) (Math.pow(x2,2)+(8*x2)+12);
+        System.out.println(x2 +"^2+8" + "(" + x2 + ")" + "+12=" + temp);
     }
     
 }
