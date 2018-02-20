aa1449338207a25b55d81c145c2297499c11701f
Juan José Rangel <juanjose840@yahoo.com.co>
2018-02-19 23:17:06 -0500

    Version 0
    Prueba de variables "byte".

---
+++ CalculaEcuacion/src/calculaecuacion/CalculaEcuacion.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calculaecuacion;
+
+/**
+ *
+ * @author JuanJ
+ */
+public class CalculaEcuacion {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        byte a = 1;
+        byte b = 8;
+        byte c = 12;
+        byte x1 = 0;
+        byte x2 = 0;
+        
+        x1 = (byte) -a; 
+        System.out.println(x1);
+    }
+    
+}
